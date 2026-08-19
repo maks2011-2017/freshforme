9b9bf7bff8a55e713bb9787da40014c945dbc1e9
losba78rf2 <randomlbpstuff@gmail.com>
2026-08-19 15:41:17 +1100

    g

--- Refresh.Core/RateLimits/Photos/SinglePhotoEndpointLimits.cs
+++ Refresh.Core/RateLimits/Photos/SinglePhotoEndpointLimits.cs
@@ -3,7 +3,7 @@ namespace Refresh.Core.RateLimits.Photos;
 public static class SinglePhotoEndpointLimits
 {
     public const int TimeoutDuration = 450;
-    public const int RequestAmount = 20;
+    public const int RequestAmount = 230;
     public const int BlockDuration = 300;
     public const string RequestBucket = "single-photo";
 }
