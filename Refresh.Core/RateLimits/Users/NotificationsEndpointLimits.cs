ad0e1c72ed9ce5fe46474aa530afa1b7860419ca
losba78rf2 <randomlbpstuff@gmail.com>
2026-08-20 01:17:57 +1100

    ggg

--- Refresh.Core/RateLimits/Users/NotificationsEndpointLimits.cs
+++ Refresh.Core/RateLimits/Users/NotificationsEndpointLimits.cs
@@ -3,9 +3,9 @@ namespace Refresh.Core.RateLimits.Users;
 public static class NotificationsEndpointLimits
 {
     public const int TimeoutDuration = 180;
-    public const int GameRequestAmount = 8;
+    public const int GameRequestAmount = 800;
     public const int ApiRequestAmount = 20;
-    public const int BlockDuration = 150;
+    public const int BlockDuration = 10;
     public const string GameRequestBucket = "notifications-game";
     public const string ApiRequestBucket = "notifications-api";
 }
