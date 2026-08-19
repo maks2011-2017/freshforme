4cd532e1555914f3d49c04942e70dce81b46aca8
losba78rf2 <randomlbpstuff@gmail.com>
2026-08-19 15:26:07 +1100

    bumpup

--- Refresh.Core/RateLimits/Comments/CommentListEndpointLimits.cs
+++ Refresh.Core/RateLimits/Comments/CommentListEndpointLimits.cs
@@ -3,7 +3,7 @@ namespace Refresh.Core.RateLimits.Comments;
 public static class CommentListEndpointLimits
 {
     public const int TimeoutDuration = 420;
-    public const int RequestAmount = 40;
+    public const int RequestAmount = 400;
     public const int BlockDuration = 300;
     public const string RequestBucket = "comment-list";
 }
--- Refresh.Core/RateLimits/Users/SingleUserEndpointLimits.cs
+++ Refresh.Core/RateLimits/Users/SingleUserEndpointLimits.cs
@@ -3,7 +3,7 @@ namespace Refresh.Core.RateLimits.Users;
 public static class SingleUserEndpointLimits
 {
     public const int TimeoutDuration = 180;
-    public const int ApiRequestAmount = 30;
+    public const int ApiRequestAmount = 300;
     public const int GameRequestAmount = 50;
     public const int BlockDuration = 120;
     public const string ApiRequestBucket = "single-user-api";
