88398425b80018debf43e3b4330ed9888852cf81
losba78rf2 <randomlbpstuff@gmail.com>
2026-08-22 01:43:22 +1100

    Update AnnouncementEndpoints.cs

--- Refresh.Interfaces.Game/Endpoints/AnnouncementEndpoints.cs
+++ Refresh.Interfaces.Game/Endpoints/AnnouncementEndpoints.cs
@@ -48,9 +48,8 @@ public class AnnouncementEndpoints : EndpointGroup
         IEnumerable<GameAnnouncement> announcements = database.GetAnnouncements().ToList();
         foreach (GameAnnouncement announcement in announcements)
             output.Append($"""
-            ======================
-            ** {announcement.Title} **
-            ======================
+            ----> {announcement.Title} <----
+            
             {announcement.Text}
 
 
